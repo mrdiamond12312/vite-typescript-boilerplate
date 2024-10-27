ab70a2ec596d658ddd4b44cf1ac86a8022befe8f
Nice <hoangkimc93@gmail.com>
2024-10-27 16:47:37 +0700

    chore(eslint): add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,48 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'import/order': [
+      2,
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern:
+              '{@(@/actions|@/app|@/assets|@/calls|@/client|@/components|@/constants|/@context|@/database|@helpers|@/hooks|@/init|@/managers|@/queries|@/screens|@/selectors|@/share|@/store|@/telemetry|@/typings|@/test|@/utils)/**,@(@/constants|@/i18n|@/notifications|@/store|@/websocket)}',
+            group: 'parent',
+            position: 'after',
+          },
+          {
+            pattern: 'app/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ['type'],
+      },
+    ],
     '@typescript-eslint/no-explicit-any': 'off',
+  },
+
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/extensions': ['.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+        moduleDirectory: ['./src/*', './src/.umi/*', 'node_modules'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
   },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { StyledEngineProvider } from "@mui/material";
 import { Provider } from "react-redux";
 
 import { router } from "@/../configs/routes";
 import store from "@/storage/store";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/global.less";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <StyledEngineProvider injectFirst>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
         <RouterProvider router={router} />
-      </StyledEngineProvider>
-    </React.StrictMode>
+      </React.StrictMode>
+    </QueryClientProvider>
   </Provider>
 );
